63045d3466bb76c22417fcb17181fd0b54112d36
Manuele Capacci <manuele@ourfir.es>
2023-04-14 16:13:59 +0200

    show more block info

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import { Alchemy, Network } from 'alchemy-sdk';
-import { useEffect, useState } from 'react';
+import { Alchemy, Network } from "alchemy-sdk";
+import { useEffect, useState } from "react";
 
-import './App.css';
+import "./App.css";
 
 // Refer to the README doc for more information about using API
 // keys in client-side code. You should never do this in production
@@ -10,7 +10,6 @@ const settings = {
   apiKey: process.env.REACT_APP_ALCHEMY_API_KEY,
   network: Network.ETH_MAINNET,
 };
-
 
 // In this week's lessons we used ethers.js. Here we are using the
 // Alchemy SDK is an umbrella library with several different packages.
@@ -21,6 +20,8 @@ const alchemy = new Alchemy(settings);
 
 function App() {
   const [blockNumber, setBlockNumber] = useState();
+  const [blockInfo, setBlockInfo] = useState();
+  const [blockTransactions, setBlockTransactions] = useState();
 
   useEffect(() => {
     async function getBlockNumber() {
@@ -30,7 +31,43 @@ function App() {
     getBlockNumber();
   });
 
-  return <div className="App">Block Number: {blockNumber}</div>;
+  useEffect(() => {
+    async function getBlockInfo() {
+      setBlockInfo(await alchemy.core.getBlock(blockNumber));
+    }
+
+    getBlockInfo();
+  }, [blockNumber]);
+
+  useEffect(() => {
+    async function getBlockTransactions() {
+      setBlockTransactions(
+        await alchemy.core.getBlockWithTransactions(blockNumber)
+      );
+    }
+
+    getBlockTransactions();
+  }, [blockNumber]);
+
+  return (
+    <>
+      <h2 className="App">Block Number: {blockNumber}</h2>
+      <div>
+        {blockInfo && (
+          <>
+            <p>{blockInfo.hash}</p>
+            <p>{blockInfo.miner}</p>
+            <p>{blockInfo.nonce}</p>
+            <p>{blockInfo.number}</p>
+            <p>{blockInfo.parentHash}</p>
+            <p>{blockInfo.timestamp}</p>
+            <p>{blockInfo.transactions.length}</p>
+          </>
+        )}
+        {console.log(blockTransactions)}
+      </div>
+    </>
+  );
 }
 
 export default App;
